38c47c13bb3f2972120c14463763f245693bef9b
Cognomech <cognomech@gmail.com>
2017-08-28 11:59:37 +0100

    Added comments, changed "!points add" to "!points change" and added negative number checks as appropriate, renamed db file

--- commands/points.js
+++ commands/points.js
@@ -6,7 +6,7 @@ module.exports.run = (client, message, args) => {
     return;
   }
 
-  if (args[0] === "add") {
+  if (args[0] === "change") {
     if (args.length !== 4) { // Args number check
       message.channel.send(`Unexpected number of arguments found: expected 4, found ${args.length}`);
       return;
@@ -15,16 +15,12 @@ module.exports.run = (client, message, args) => {
       message.channel.send("Expected number for third argument.");
       return;
     }
-    if (args[2] < 0) { // Negative number check
-      message.channel.send("Use \"!points remove\" to remove points.");
-      return;
-    }
     if (args[2] === "0") { // Zero check
-      message.channel.send("You cannot add 0 points.");
+      message.channel.send("You cannot make a change of 0 points.");
       return;
     }
 
-    const db = new sql.Database("./databases/PointsDB", (err) => { // Open database connection
+    const db = new sql.Database("./databases/Points.db", (err) => { // Open database connection
       if (err) {
         console.log(err);
         return;
@@ -43,6 +39,11 @@ module.exports.run = (client, message, args) => {
           }
 
           if (!result) { // If user not found in table
+            if (args[2] < 0) { // Negative number check
+              message.channel.send(`You cannot subtract points from ${args[1]} because they are not currently being tracked.`);
+              return;
+            }
+
             const botMessage = await message.channel.send(`${args[1]} is not currently being tracked. Would you like to initiate tracking?`); // Set up reaction menu
             await botMessage.react("✅");
             await botMessage.react("❎");
@@ -64,7 +65,7 @@ module.exports.run = (client, message, args) => {
                     return;
                   }
 
-                  db.run(`CREATE TABLE IF NOT EXISTS ${args[1]} (time text PRIMARY KEY, pointschange integer, log text)`, [], (err6) => {
+                  db.run(`CREATE TABLE IF NOT EXISTS ${args[1]} (time text PRIMARY KEY, pointschange integer, log text)`, [], (err6) => { // Creates new table for user logs if not already there (it shouldn't be)
                     if (err) {
                       console.log(err6);
                       return;
@@ -73,7 +74,7 @@ module.exports.run = (client, message, args) => {
                     const dateObj = new Date();
                     const date = dateObj.toUTCString();
 
-                    db.run(`INSERT INTO ${args[1]} (time, pointschange, log) VALUES ("${date}", ${args[2]}, "${args[3]}")`, [], (err1) => {
+                    db.run(`INSERT INTO ${args[1]} (time, pointschange, log) VALUES ("${date}", ${args[2]}, "${args[3]}")`, [], (err1) => { // Write new log into table "username"
                       if (err1) {
                         console.log(err1);
                       }
@@ -90,13 +91,17 @@ module.exports.run = (client, message, args) => {
               }
             });
           } else { // User has been found in table
-            db.get(`SELECT points points FROM users WHERE name = "${args[1]}"`, [], (err4, row) => {
+            db.get(`SELECT points points FROM users WHERE name = "${args[1]}"`, [], (err4, row) => { // Grab points value from table "users"
               if (err4) {
                 console.log(err4);
               }
 
               const newpoints = +row.points + +args[2];
-              db.run(`UPDATE users SET points = ${newpoints} WHERE name = "${args[1]}"`, [], (err5) => {
+              if (newpoints < 0) { // Negative number check
+                message.channel.send("This change would result in a negative point value, which is not allowed.");
+                return;
+              }
+              db.run(`UPDATE users SET points = ${newpoints} WHERE name = "${args[1]}"`, [], (err5) => { // Update points for user in table "users"
                 if (err5) {
                   console.log(err5);
                 }
@@ -104,14 +109,14 @@ module.exports.run = (client, message, args) => {
                 const dateObj = new Date();
                 const date = dateObj.toUTCString();
 
-                db.run(`INSERT INTO ${args[1]} (time, pointschange, log) VALUES ("${date}", ${args[2]}, "${args[3]}")`, [], (err1) => {
+                db.run(`INSERT INTO ${args[1]} (time, pointschange, log) VALUES ("${date}", ${args[2]}, "${args[3]}")`, [], (err1) => { // Write new log into table "username"
                   if (err1) {
                     console.log(err1);
                   }
 
                   message.channel.send(`${args[1]} now has ${newpoints} points after being awarded ${args[2]} for: ${args[3]}`);
 
-                  db.close((err6) => {
+                  db.close((err6) => { // Close the database
                     if (err6) {
                       console.log(err6);
                     }
