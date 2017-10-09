cd5d8a946fce20e3004c12053b11e5ffde77c2b1
Cognomech <cognomech@gmail.com>
2017-10-09 09:02:09 +0100

    New rename command!

---
+++ commands/points/rename.js
@@ -0,0 +1,45 @@
+const cmdUtils = require("../../cogLib/cmdUtils.js");
+const sqlite = require("sqlite");
+
+module.exports = new cmdUtils.HelperCommand(
+  require("./points.js"),
+
+  {
+    name: "rename",
+  },
+
+  {
+    roleIDs: ["277880534561062912", "298843020558598154"],
+  },
+
+  async (client, message, args) => {
+    const oldName = args[0];
+    const newName = args[1];
+
+    if (args.length !== 2) {
+      message.channel.send("Expected format: !points rename [old name] [new name]");
+      return;
+    }
+
+    const db = await sqlite.open("databases/points.db");
+
+
+    let testEntry = await db.get(`SELECT name name, points points FROM main WHERE name = "${oldName}"`);
+    if (!testEntry) {
+      message.channel.send(`Could not find ${oldName}`);
+      await db.close();
+      return;
+    }
+    testEntry = await db.get(`SELECT name name, points points FROM main WHERE name = "${newName}"`);
+    if (testEntry) {
+      message.channel.send(`${newName} is already being tracked.`);
+      await db.close();
+      return;
+    }
+
+    await db.run(`UPDATE main SET name = "${newName}" WHERE name = "${oldName}"`);
+    await db.run(`ALTER TABLE "${oldName}" RENAME TO "${newName}"`);
+    await db.close();
+    message.channel.send(`${oldName} successfully renamed to ${newName}`);
+  }
+);
