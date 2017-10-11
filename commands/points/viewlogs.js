f9efe3bd0f0237118c1726929a334c8334d2a7e8
Cognomech <cognomech@gmail.com>
2017-10-12 00:00:53 +0100

    New command to view point logs!

---
+++ commands/points/viewlogs.js
@@ -0,0 +1,77 @@
+const cmdUtils = require("../../cogLib/cmdUtils.js");
+const sqlite = require("sqlite");
+const discord = require("discord.js");
+const clientUtils = require("../../cogLib/clientUtils.js");
+
+module.exports = new cmdUtils.HelperCommand(
+  require("./points.js"),
+
+  {
+    name: "viewlogs",
+  },
+
+  {},
+
+  (async (client, message, args) => {
+    const db = await sqlite.open("databases/points.db");
+    const username = args[0];
+    const totalRows = (await db.get(`SELECT count(*) FROM "${username}"`))["count(*)"];
+    const rowsPerPage = 10;
+    let pageNumber = 0;
+
+    async function grabLogs() {
+      const logArray = await db.all(`
+        SELECT readableTime readableTime, pointsChange pointsChange, log log 
+        FROM "${username}"
+        ORDER BY epochTime DESC
+        LIMIT ${rowsPerPage}
+        OFFSET ${10 * pageNumber}`
+      );
+
+      let logString = "";
+      for (let i = 0; i < logArray.length; i += 1) {
+        const row = logArray[i];
+        logString += `[${row.readableTime}]: ${row.pointsChange} for ${row.log}\n`;
+      }
+      logString = `\`\`\`${logString}\`\`\``;
+      return logString;
+    }
+    async function generateEmbed() {
+      return new discord.RichEmbed()
+        .setColor(0xaf00ff)
+        .setThumbnail(`http://services.runescape.com/m=avatar-rs/a=13/${username}/chat.png`)
+        .setTimestamp()
+        .setTitle(`__${username}'s Clan Points Logs__`)
+        .setDescription(await grabLogs())
+        .setFooter(`
+          Showing ${(10 * pageNumber) + 1}-
+          ${(pageNumber + 1) * 10 < totalRows ? (10 * pageNumber) + 10 : totalRows} of ${totalRows} rows
+        `);
+    }
+    const embed = await generateEmbed();
+
+    const clientMessage = await message.channel.send({ embed });
+    async function updateMessage(pageNum) {
+      pageNumber = pageNum;
+      const newEmbed = await generateEmbed();
+      await clientMessage.edit(undefined, newEmbed);
+    }
+
+    await clientUtils.reactLeftRightEndMenu(client,
+      message,
+      clientMessage,
+      () => {
+        if (pageNumber > 0) {
+          updateMessage(pageNumber - 1);
+        }
+      },
+      () => {
+        if ((pageNumber + 1) * 10 < totalRows) {
+          updateMessage(pageNumber + 1);
+        }
+      }
+    );
+
+    db.close();
+  })
+);
