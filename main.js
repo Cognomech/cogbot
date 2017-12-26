f807dfc6e2e37b1c4a4285cb9a64105f809e39b9
Cognomech <cognomech@gmail.com>
2017-12-26 07:11:41 +0000

    Added dev option to prevent unnecessary disruption of standard operation

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const discord = require("discord.js");
 const cmdUtils = require("./cogLib/cmdUtils.js");
 const config = require("./config.json");
 const eventUtils = require("./cogLib/eventUtils.js");
-const leaderboard = require("./cogLib/leaderboard.js");
+const rankUtils = require("./cogLib/rankUtils.js");
+
+const dev = false;
 
 const client = new discord.Client();
 client.cmdHandler = new cmdUtils.CommandHandler(`${__dirname}/commands`);
@@ -14,6 +16,8 @@ client.eventHandler = new eventUtils.EventHandler(client, `${__dirname}/events`)
     client.eventHandler.registerEventsFromDir()
   ]);
   await client.login(config.token);
-  await leaderboard.update(client.guilds.get("277869752867749888"), "databases/points.db");
+  if (!dev) {
+    await rankUtils.updateLB(client.guilds.get("277869752867749888"), "databases/points.db");
+  }
 })();
 
