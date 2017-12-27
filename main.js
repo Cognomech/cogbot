17def41426ac36c66673af88ca8bf7323aaa8641
Cognomech <cognomech@gmail.com>
2017-12-27 09:27:48 +0000

    Moved dev option to config

--- main.js
+++ main.js
@@ -4,8 +4,6 @@ const config = require("./config.json");
 const eventUtils = require("./cogLib/eventUtils.js");
 const rankUtils = require("./cogLib/rankUtils.js");
 
-const dev = false;
-
 const client = new discord.Client();
 client.cmdHandler = new cmdUtils.CommandHandler(`${__dirname}/commands`);
 client.eventHandler = new eventUtils.EventHandler(client, `${__dirname}/events`);
@@ -16,8 +14,8 @@ client.eventHandler = new eventUtils.EventHandler(client, `${__dirname}/events`)
     client.eventHandler.registerEventsFromDir()
   ]);
   await client.login(config.token);
-  if (!dev) {
-    await rankUtils.updateLB(client.guilds.get("277869752867749888"), "databases/points.db");
+  if (!config.dev) {
+    await rankUtils.update(client.guilds.get("277869752867749888"), "databases/points.db");
   }
 })();
 
