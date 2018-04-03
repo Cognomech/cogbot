32f744d44bfbb339546e43fe2280e147e636ed98
Cognomech <cognomech@gmail.com>
2018-04-03 04:11:43 +0100

    Creayed mathsUtils with unicode and text representations of numbers

--- cogLib/mathsUtils.js
+++ cogLib/mathsUtils.js
@@ -9,3 +9,7 @@ module.exports.percentile = function percentile(numbers, percent) {
   index = Math.ceil(index);
   return numbers[index - 1];
 };
+
+module.exports.numberText = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+module.exports.numberUnicode = ["\u0030\u20E3", "\u0031\u20E3", "\u0032\u20E3", "\u0033\u20E3", "\u0034\u20E3", "\u0035\u20E3", "\u0036\u20E3", "\u0037\u20E3", "\u0038\u20E3", "\u0039\u20E3"];
