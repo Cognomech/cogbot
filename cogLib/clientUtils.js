31413451dab47721801e7128883feb5e088105ed
Cognomech <cognomech@gmail.com>
2018-04-03 04:10:50 +0100

    Added modular numbered menu

--- cogLib/clientUtils.js
+++ cogLib/clientUtils.js
@@ -1,4 +1,5 @@
 const discord = require("discord.js");
+const mathsUtils = require("./mathsUtils.js");
 
 function reactionFilter(clientMessage, userMessage, allowedReactions) {
   const collector = clientMessage.createReactionCollector(() => true);
@@ -92,6 +93,62 @@ module.exports.reactLeftRightEndMenu = (client, userMessage, clientMessageArg, l
     responseCollector.on("end", async (collected, reason) => {
       if (!(reason === "user")) {
         await clientMessage.clearReactions();
+        filterCollector.stop();
+        resolve();
+      }
+    });
+  }
+
+  collectorFunc();
+});
+
+module.exports.reactNumberedMenu = (client, userMessage, clientMessageArg, funcs, timeout = 300000) => new Promise(async (resolve) => {
+  let clientMessage;
+  const emojis = [];
+  if (typeof clientMessageArg === "string") {
+    clientMessage = await userMessage.channel.send(clientMessageArg);
+  } else {
+    clientMessage = clientMessageArg;
+  }
+  /* eslint-disable */
+  // These NEED to be done in a particular order
+  for (let i = 1; i <= funcs.length; i += 1) {
+    emojis.push(mathsUtils.numberUnicode[i]);
+    await clientMessage.react(mathsUtils.numberUnicode[i]);
+  }
+  /* eslint-enable */
+
+  const filterCollector = reactionFilter(clientMessage, userMessage, emojis);
+  let responseCollector;
+
+  async function collectorFunc(reaction) {
+    if (reaction !== undefined && emojis.includes(reaction.emoji.name)) {
+      const i = Number.parseInt(reaction.emoji.name.slice(0, 1), 10);
+      const requestEnd = funcs[i - 1]();
+      await reaction.remove(userMessage.author);
+      if (requestEnd) {
+        responseCollector.stop("Request End");
+        return;
+      }
+      responseCollector.stop();
+    }
+
+    responseCollector = clientMessage.createReactionCollector(
+      (collected, user) => ((user === userMessage.author) && (emojis.includes(collected.emoji.name))),
+      {
+        time: timeout,
+      }
+    );
+    responseCollector.on("collect", (response) => {
+      collectorFunc(response);
+    });
+    responseCollector.on("end", async (collected, reason) => {
+      if (!(reason === "user")) {
+        clientMessage.clearReactions();
+        if (reason === "time") {
+          clientMessage.delete();
+          userMessage.delete();
+        }
         filterCollector.stop();
         resolve();
       }
